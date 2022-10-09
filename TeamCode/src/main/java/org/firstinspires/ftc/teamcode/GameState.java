98eea0db78d3ca5d151d1c32f1ba76fdd5202938
The1AndOnlyAlex <alex.l.duan@gmail.com>
2022-10-08 20:51:37 -0700

    Game state machine

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GameState.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+public class GameState {
+
+    public final static int BEFORE_RUN = 0;
+    public final static int RUN = 1;
+    public final static int CHECK_TIME = 2;
+    public final static int ERROR_RUN = 3;
+}
