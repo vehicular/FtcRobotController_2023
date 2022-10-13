1d72e64bbb393d6d904a1bedc864da7facfd140e
The1AndOnlyAlex <40921264+The1AndOnlyAlex@users.noreply.github.com>
2022-10-12 22:28:23 -0700

    relocated file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -1,11 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.core.Robot;
 import org.firstinspires.ftc.teamcode.core.Subsystem;
+import org.firstinspires.ftc.teamcode.util.GameState;
 
 @TeleOp(name="JD Robot",group="")
 //@Disabled
